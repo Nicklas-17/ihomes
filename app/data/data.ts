4e61bf86820a3b311771e5764145e028761bd253
Nicklas <nick.97k@gmail.com>
2024-07-23 23:18:28 +0000

    data fixed

--- app/data/data.ts
+++ app/data/data.ts
@@ -56,3 +56,76 @@ export const teamMembers: teamMember[] = [
     description: "Sophia handles all surveying tasks and ensures accuracy.",
   },
 ];
+
+export const houseOptions = [
+  {
+    id: 1,
+    title: "Modern Family Home",
+    price: "$500,000",
+    description: "A beautiful modern home with 4 bedrooms and 3 bathrooms.",
+    imageUrl: "/images/AstoriaVillas.png",
+  },
+  {
+    id: 2,
+    title: "Luxury Villa",
+    price: "$1,200,000",
+    description: "A luxurious villa with a private pool and garden.",
+    imageUrl: "/images/BeverlyCourt.png",
+  },
+  {
+    id: 3,
+    title: "Cozy Cottage",
+    price: "$350,000",
+    description: "A cozy cottage perfect for a small family.",
+    imageUrl: "/images/HouseOne.png",
+  },
+  {
+    id: 4,
+    title: "Cozy Cottage",
+    price: "$350,000",
+    description: "A cozy cottage perfect for a small family.",
+    imageUrl: "/images/SkyVillaLakeside.png",
+  },
+  {
+    id: 5,
+    title: "Cozy Cottage",
+    price: "$350,000",
+    description: "A cozy cottage perfect for a small family.",
+    imageUrl: "/images/LynsBridge.png",
+  },
+  {
+    id: 6,
+    title: "Cozy Cottage",
+    price: "$350,000",
+    description: "A cozy cottage perfect for a small family.",
+    imageUrl: "/images/LynsBridge.png",
+  },
+  {
+    id: 7,
+    title: "Cozy Cottage",
+    price: "$350,000",
+    description: "A cozy cottage perfect for a small family.",
+    imageUrl: "/images/LynsBridge.png",
+  },
+  {
+    id: 8,
+    title: "Cozy Cottage",
+    price: "$350,000",
+    description: "A cozy cottage perfect for a small family.",
+    imageUrl: "/images/LynsBridge.png",
+  },
+  {
+    id: 9,
+    title: "Cozy Cottage",
+    price: "$350,000",
+    description: "A cozy cottage perfect for a small family.",
+    imageUrl: "/images/LynsBridge.png",
+  },
+  {
+    id: 10,
+    title: "Cozy Cottage",
+    price: "$350,000",
+    description: "A cozy cottage perfect for a small family.",
+    imageUrl: "/images/LynsBridge.png",
+  },
+];
